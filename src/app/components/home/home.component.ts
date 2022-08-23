4c464c0b5e76976ae9dca89430b76220c3530fd8
özcan çetin <cetin445@gmail.com>
2022-08-23 20:26:37 +0300

    directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  // users:string[] = []
+  users:{ name: string; age:number}[] | undefined ;
+  // age: {name: string, age:number}[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    // this.users = ['ali', 'veli', 'deli'];
 
+    this.users = [
+      {
+        name:'veli',
+        age:10
+      },
+      {
+        name:'ali',
+        age:6
+      },
+      {
+        name:'ömer',
+        age:7
+      }
+    ]
+
+  }
 }
